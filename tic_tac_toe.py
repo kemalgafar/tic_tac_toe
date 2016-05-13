6782703f26142389292f3ecfa2f1ead01e42c924
Kemal Gafar <kemalgafar@gmail.com>
2016-05-13 18:10:22 -0400

    Add files via upload
    All funcs are internal to the class now

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,7 +2,7 @@
 # TODONE 2. Write a better display func, show the board properly
 # TODO 3. Refactor functions, remove import random -> use dicts for lists
 # TODO 4. PEP8 cleanup
-# TODO 5. Ask whether the user wants to go first or not (will need to rewite O's decsions)
+# TODO 5. Ask whether the user wants to go first or not (will need to rewite O's decsions?)
 # TODO 6. Write unit tests
 
 import re
@@ -12,6 +12,11 @@ class Board(object):
     def __init__(self):
         self.spaces = [[" " for i in range(3)] for j in range(3)]
         self.turn_ctr = 0
+        self.char_look_up = {"A":0, "B":1, "C":2}
+        # The 9 possible spaces
+        self.corners = [[0,0], [0,2], [2,0], [2,2]] #make into dicts, then no need for importing rand
+        self.edges = [[0,1], [1,0], [1,2], [2,1]]
+        self.center = [[1,1]]
 
     def display_board(self):
         abc = ["A", "B", "C"]
@@ -30,116 +35,109 @@ class Board(object):
         else:
             print("O won the game!")
 
-char_look_up = {"A":0, "B":1, "C":2}
-
-# The 9 possible spaces
-corners = [[0,0], [0,2], [2,0], [2,2]] #make into dicts, then no need for importing rand
-edges = [[0,1], [1,0], [1,2], [2,1]]
-center = [[1,1]]
-
-def place_x_o(Game_bd):
-    if Game_bd.turn_ctr % 2 == 0:
-        x_turn(Game_bd)
-    else:
-        o_turn(Game_bd)
-
-def x_turn(Game_bd):
-    x_coord = str_to_int()
-    if Game_bd.spaces[x_coord[0]][x_coord[1] - 1] == " ":
-        if [x_coord[0], x_coord[1] - 1] in corners:
-            corners.remove([x_coord[0], x_coord[1] - 1])
-        elif [x_coord[0], x_coord[1] - 1] in edges:
-            edges.remove([x_coord[0], x_coord[1] - 1])
+    def place_x_o(self):
+        if self.turn_ctr % 2 == 0:
+            self.x_turn()
         else:
-            center.remove([x_coord[0], x_coord[1] - 1])
-        Game_bd.spaces[x_coord[0]][x_coord[1] - 1] = "X"
-    else:
-        print("That space is already occupied! Pick another square")
-        x_turn(Game_bd)
+            self.o_turn()
 
-def user_input():
-    print("X's turn!\nPlease enter in a coordinate to place an X")
-    user_string = input(">  ")
-    return user_string
-
-def check_input():
-    str = user_input().upper()
-    matches = re.search(r'([a-cA-C])([1-3])', str)
-    if matches:
-        return [match for match in matches.groups()]
-    else:
-        print("Erroneous input, please try again")
-        return check_input()
-
-def str_to_int():
-    str_coords = check_input()
-    int_list = []
-    int_list.append(char_look_up[str_coords[0]])
-    int_list.append(int(str_coords[1]))
-    return int_list
-
-def o_turn(Game_bd):
-    print("O's turn!")
-    o_coord = o_choices(Game_bd)
-    Game_bd.spaces[o_coord[0]][o_coord[1]] = "O"
-
-def o_choices(Game_bd):
-    # Opening move for "O"
-    if Game_bd.turn_ctr == 1:
-        if Game_bd.spaces[1][1] == "X":
-            random.shuffle(corners)
-            return corners.pop()
+    def x_turn(self):
+        x_coord = self.str_to_int()
+        if self.spaces[x_coord[0]][x_coord[1] - 1] == " ":
+            if [x_coord[0], x_coord[1] - 1] in self.corners:
+                self.corners.remove([x_coord[0], x_coord[1] - 1])
+            elif [x_coord[0], x_coord[1] - 1] in self.edges:
+                self.edges.remove([x_coord[0], x_coord[1] - 1])
+            else:
+                self.center.remove([x_coord[0], x_coord[1] - 1])
+            self.spaces[x_coord[0]][x_coord[1] - 1] = "X"
         else:
-            return center.pop()
-    # Every other move for "0"
-    #elif:
-    else:
-        remaining_spaces = (corners + edges + center)
-        try:
-            remaining_spaces.remove([])
-        except:
-            pass
-        return random.choice(remaining_spaces)
+            print("That space is already occupied! Pick another square")
+            self.x_turn()
+
+    def user_input(self):
+        print("X's turn!\nPlease enter in a coordinate to place an X")
+        user_string = input(">  ")
+        return user_string
+
+    def check_input(self):
+        str = self.user_input().upper()
+        matches = re.search(r'([a-cA-C])([1-3])', str)
+        if matches:
+            return [match for match in matches.groups()]
+        else:
+            print("Erroneous input, please try again")
+            return self.check_input()
+
+    def str_to_int(self):
+        str_coords = self.check_input()
+        int_list = []
+        int_list.append(self.char_look_up[str_coords[0]])
+        int_list.append(int(str_coords[1]))
+        return int_list
+
+    def o_turn(self):
+        print("O's turn!")
+        o_coord = self.o_choices()
+        self.spaces[o_coord[0]][o_coord[1]] = "O"
+
+    def o_choices(self):
+        # Opening move for "O"
+        if self.turn_ctr == 1:
+            if self.spaces[1][1] == "X":
+                random.shuffle(corners)
+                return self.corners.pop()
+            else:
+                return self.center.pop()
+        # Every other move for "0"
+        #elif:
+        else:
+            remaining_spaces = (self.corners + self.edges + self.center)
+            try:
+                remaining_spaces.remove([])
+            except:
+                pass
+            return random.choice(remaining_spaces)
 
 
-def score_board(Game_bd):
-    three_in_a_row = False
-    if Game_bd.turn_ctr % 2 == 0:
-        token = "X"
-    else:
-        token = "O"
-    #Score diagonals
-    if Game_bd.spaces[1][1] == token:
-        if (Game_bd.spaces[0][0] == token) and (Game_bd.spaces[2][2] == token):
-            three_in_a_row = True
-        if (Game_bd.spaces[0][2] == token) and (Game_bd.spaces[2][0] == token):
-            three_in_a_row = True
-    #Score horizontals
-    for i in range(3):
-        if (Game_bd.spaces[i][0] == token) and (Game_bd.spaces[i][1] == token) and (Game_bd.spaces[i][2] == token):
-            three_in_a_row = True
-    #Score verticals
-    for j in range(3):
-        if (Game_bd.spaces[0][j] == token) and (Game_bd.spaces[1][j] == token) and (Game_bd.spaces[2][j] == token):
-            three_in_a_row = True
-    return three_in_a_row
+    def score_board(self):
+        three_in_a_row = False
+        if self.turn_ctr % 2 == 0:
+            token = "X"
+        else:
+            token = "O"
+        #Score diagonals
+        if self.spaces[1][1] == token:
+            if (self.spaces[0][0] == token) and (self.spaces[2][2] == token):
+                three_in_a_row = True
+            if (self.spaces[0][2] == token) and (self.spaces[2][0] == token):
+                three_in_a_row = True
+        #Score horizontals
+        for i in range(3):
+            if (self.spaces[i][0] == token) and (self.spaces[i][1] == token) and (self.spaces[i][2] == token):
+                three_in_a_row = True
+        #Score verticals
+        for j in range(3):
+            if (self.spaces[0][j] == token) and (self.spaces[1][j] == token) and (self.spaces[2][j] == token):
+                three_in_a_row = True
+        return three_in_a_row
 
-def play_game(Game_bd):
-    win = False
-    while Game_bd.turn_ctr < 10:
-        Game_bd.display_board()
-        place_x_o(Game_bd)
-        if Game_bd.turn_ctr > 3:
-            win = score_board(Game_bd)
-            if win == True:
-                Game_bd.display_board()
-                Game_bd.display_winner()
+    def play_game(self):
+        win = False
+        while self.turn_ctr < 10:
+            self.display_board()
+            self.place_x_o()
+            if self.turn_ctr > 3:
+                win = self.score_board()
+                if win == True:
+                    self.display_board()
+                    self.display_winner()
+                    break
+            self.turn_ctr += 1
+            if self.turn_ctr == 9 and win == False:  #take the dis winner func out of the class can combine all 3 options in a func
+                print("The game ends in a stalemate")
                 break
-        Game_bd.turn_ctr += 1
-        if Game_bd.turn_ctr == 9 and win == False:  #take the dis winner func out of the class can combine all 3 options in a func
-            print("The game ends in a stalemate")
-            break
 
 
 Game_bd = Board()
-play_game(Game_bd)
+Game_bd.play_game()
